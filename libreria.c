a662e6aae970f0beb817a87bcc71cf819af3de31
ivan <i.pina.2023@alumnos.urjc.es>
2025-11-03 19:11:39 +0100

    tail function done.

--- libreria.c
+++ libreria.c
@@ -14,3 +14,35 @@ int head(int N) {
     }
     return 0;
 }
+
+int tail(int N){
+    char **lines = malloc(N * sizeof(char *));
+    for (int i = 0; i < N; i++) {
+        lines[i] = NULL;
+    }
+    char line[1024];
+    int count = 0;
+    while (fgets(line, sizeof(line), stdin) != NULL) {
+        int position = count % N;
+        free(lines[position]);
+        lines[position] = strdup(line);
+        count ++;
+    }
+
+    int total = N;
+    if (count < N) {
+        total = count;
+    }
+
+    int start = 0;
+    if (count > N) {
+        start = count % N;
+    }
+    for (int i = 0; i < total; i++) {
+        int index = (i + start) % N;
+        fputs(lines[index], stdout);
+        free(lines[index]);
+    }
+    free(lines);
+    return 0;
+}
